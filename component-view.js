a0d201bc63dde3abb7788c2bacebad0c807a8ca1
SE Kelly <94656500+sekelly@users.noreply.github.com>
2026-04-30 17:50:19 -0700

    Update component-view.js
    code

--- component-view.js
+++ component-view.js
@@ -1,53 +1,66 @@
-const client = ZoomMtgEmbedded.createClient()
+const client = ZoomMtgEmbedded.createClient();
 
-let meetingSDKElement = document.getElementById('meetingSDKElement')
+let meetingSDKElement = document.getElementById("meetingSDKElement");
 
-// setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-var signatureEndpoint = 'https://meetingsdk-sample-signature-node-js-iota.vercel.app/'
-var sdkKey = 'PfPapLCavJc2ZBjMkssNeqboLdBOpoAEXQTc'
-var meetingNumber = '95160050210'
-var role = 1
-var userName = 'Component'
-var userEmail = 'madmax@kelmagaoay.com'
-var passWord = '123'
-// pass in the registrant's token if your meeting or webinar requires registration. More info here:
-// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings#join-meeting-with-registration-required
-// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/webinars#join-webinar-with-registration-required
-// var registrantToken = '7YSlcuU2b174wkJAjHvnhIVBRthvhFQnsPimWiqjiq0.DQcAAAATt_8ZOxZQMHQ5NXo1MFQ1TzZYczkwYVBWcXdnAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&pwd=iXwcQPrqlBGhimrDLYLNr6yaEwOwSu.1'
-
-client.init({
-  zoomAppRoot: meetingSDKElement,
-  language: 'en-US',
-})
+var authEndpoint = "https://meetingsdk-sample-signature-node-js-iota.vercel.app/";
+var meetingNumber = "95160050210";
+var passWord = "123";
+var role = 1;
+var userName = "Component View";
+var userEmail = "component@@kelkel.com";
+var registrantToken = "";
+var zakToken = "";
 
 function getSignature() {
-  fetch(signatureEndpoint, {
-    method: 'POST',
+  fetch(authEndpoint, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       meetingNumber: meetingNumber,
-      role: role
-    })
-  }).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-    startMeeting(data.signature)
-  }).catch((error) => {
-  	console.log(error)
+      role: role,
+    }),
   })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      startMeeting(data.signature);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function startMeeting(signature) {
-  client.join({
-    sdkKey: sdkKey,
-    signature: signature,
-    meetingNumber: meetingNumber,
-    password: passWord,
-    userName: userName,
-    userEmail: userEmail,
-    tk: registrantToken
-  })
+  client
+    .init({
+      zoomAppRoot: meetingSDKElement,
+      language: "en-US",
+      patchJsMedia: true,
+      leaveOnPageUnload: true,
+    })
+    .then(() => {
+      client
+        .join({
+          signature: signature,
+          meetingNumber: meetingNumber,
+          password: passWord,
+          userName: userName,
+          userEmail: userEmail,
+          tk: registrantToken,
+          zak: zakToken,
+        })
+        .then(() => {
+          console.log("joined successfully");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
